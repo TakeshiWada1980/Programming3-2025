c5bac4560bdc4bdb4ae35917f1033002c2417194
T.Wada <28886385+TakeshiWada1980@users.noreply.github.com>
2025-11-14 18:30:12 +0900

    docs: create code02.ts

---
+++ docs/codes/07/code02.ts
@@ -0,0 +1,12 @@
+// src/app/_types/Post.ts
+import type { Category } from "./Category";
+import type { CoverImage } from "./CoverImage";
+
+export type Post = {
+  id: string;
+  title: string;
+  content: string;
+  createdAt: string;
+  categories: Category[];
+  coverImage: CoverImage;
+};
