4f184d6424224b2542d06bc251ff15e1169254fa
T.Wada <28886385+TakeshiWada1980@users.noreply.github.com>
2026-01-10 14:22:50 +0900

    update 13 files and create 3 files

--- docs/codes/11/code01.tsx
+++ docs/codes/11/code01.tsx
@@ -30,10 +30,9 @@ const Page: React.FC = () => {
   const [newTitle, setNewTitle] = useState("");
   const [newContent, setNewContent] = useState("");
   const [newCoverImageURL, setNewCoverImageURL] = useState("");
-  const [newCoverImageKey, setNewCoverImageKey] = useState("hoge"); // ◀ 追加
 
+  const { token } = useAuth(); // トークンの取得
   const router = useRouter();
-  const { token } = useAuth();
 
   // カテゴリ配列 (State)。取得中と取得失敗時は null、既存カテゴリが0個なら []
   const [checkableCategories, setCheckableCategories] = useState<
@@ -113,12 +112,9 @@ const Page: React.FC = () => {
     setNewCoverImageURL(e.target.value);
   };
 
-  //
-  const updateNewCoverImage = (e: React.ChangeEvent<HTMLInputElement>) => {};
-
   // フォームの送信処理
   const handleSubmit = async (e: React.FormEvent<HTMLFormElement>) => {
-    e.preventDefault();
+    e.preventDefault(); // この処理をしないとページがリロードされるので注意
 
     // ▼ 追加: トークンが取得できない場合はアラートを表示して処理中断
     if (!token) {
@@ -128,6 +124,7 @@ const Page: React.FC = () => {
 
     setIsSubmitting(true);
 
+    // ▼▼ 追加 ウェブAPI (/api/admin/posts) にPOSTリクエストを送信する処理
     try {
       const requestBody = {
         title: newTitle,
@@ -246,29 +243,6 @@ const Page: React.FC = () => {
             required
           />
         </div>
-
-        <div className="space-y-1">
-          <label htmlFor="coverImageKey" className="block font-bold">
-            カバーイメージ (Key)
-          </label>
-          <input
-            type="url"
-            id="coverImageKey"
-            name="coverImageKey"
-            className="w-full rounded-md border-2 px-2 py-1 text-gray-500"
-            value={newCoverImageKey}
-            disabled
-            readOnly
-            required
-          />
-        </div>
-
-        <input
-          type="file"
-          id="coverImage"
-          onChange={updateNewCoverImage}
-          accept="image/*"
-        />
 
         <div className="space-y-1">
           <div className="font-bold">タグ</div>
